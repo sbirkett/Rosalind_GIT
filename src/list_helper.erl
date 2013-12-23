d2cea4ca70ea670a29cfd3b328909ad89e4b94a8
scott <supergrimey@gmail.com>
2013-12-22 20:27:32 -0600

    list helpers

--- src/list_helper.erl
+++ src/list_helper.erl
@@ -3,7 +3,15 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([swap_elements/3,reverse_subsequence/3]).
+-export([remove_longest_duplication/2,swap_elements/3,reverse_subsequence/3]).
+
+remove_longest_duplication(A,B)->
+  if
+    length(A) > length(B) -> remove_longest_duplication_rec(A,B,length(B));
+    true -> 
+      Overlap = remove_longest_duplication_rec(A,B,length(A)),
+      lists:sublist(A,length(A)-Overlap) ++ B
+  end.
 
 swap_elements(List,X,Y)->
   swap_elements_rec(List,X,Y,[],1).
@@ -17,6 +25,27 @@ reverse_subsequence(List,X,Y)->
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
+%Performed Greedy
+remove_longest_duplication_rec(A,B,FrameSize)->
+
+   First = lists:nthtail(length(A)-FrameSize,A),
+   Second = lists:sublist(B,FrameSize),
+   Dup = confirm_duplication(First++Second),
+
+   if
+       Dup -> FrameSize;
+       FrameSize == 0 -> 0;
+       true -> remove_longest_duplication_rec(A,B,FrameSize-1)
+   end.
+
+confirm_duplication(Segment)->
+  if
+    length(Segment) rem 2 /= 0 ->
+      length(Segment) == length( [ T || T <- Segment, T == hd(Segment) ] );
+    true -> 
+      lists:sublist(Segment,round(length(Segment)/2)) == lists:sublist(Segment,round((length(Segment)/2)+1),length(Segment))
+  end.
 
 swap_elements_rec(List,X,Y,Out,CurrIndex)->
   if
