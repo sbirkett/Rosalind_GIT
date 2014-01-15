62d2f99afb483a569e92a54423bb11c67087f8a6
scott <supergrimey@gmail.com>
2014-01-14 21:07:37 -0600

    another failed attempt at shared_spliced_motif)

--- src/shared_spliced_motif.erl
+++ src/shared_spliced_motif.erl
@@ -2,6 +2,7 @@
 
 -include_lib("../rosalind_records.hrl").
 
+-record(line,{startIndex,endIndex}).
 -record(problemSet,{seq,first,second}).
 
 -import(fasta_reader,[read/1]).
@@ -20,12 +21,76 @@ start(File)->
   % Read input file
   Seqs = [ T#fasta.sequence || T <- fasta_reader:read(File) ],
 
+  T = make_index_dict(lists:last(Seqs),dict:new(),1),
+  
+  Lines = build_all_lines(hd(Seqs),1,T,[]),
+
+  RemainingLines = solve(Lines),
+
+  Final = lists:foldl( fun(A,Acc) ->
+	Line = element(1,A),
+	Acc ++ [lists:nth(Line#line.startIndex,hd(Seqs))] end,[],RemainingLines),
+
+  io:format("~s\n",[Final]).
+  %WeightedLines = weight_all_lines(Lines,Lines,[]).
+
   % Build problemSet
-  FirstProblem = #problemSet{seq=[],first=hd(Seqs),second=lists:last(Seqs)},
+ % FirstProblem = #problemSet{seq=[],first=hd(Seqs),second=lists:last(Seqs)},
 
   % Profit
-  hd(lists:sort( fun(A,B) ->
-	  length(A) > length(B) end ,iterate([FirstProblem],[]))).
+  %hd(lists:sort( fun(A,B) ->
+%	  length(A) > length(B) end ,iterate([FirstProblem],[]))).
+
+solve(Lines)->
+  io:format("Lines remaining = ~w\n",[length(Lines)]),
+  WeightedLines = weight_all_lines(Lines,Lines,[]),
+  %io:format("WeightedLines = ~w\n",[WeightedLines]),
+  SortedWeightedLines = lists:sort( fun(A,B) ->
+	element(2,A) > element(2,B) end,WeightedLines),
+  case ( element(2,hd(SortedWeightedLines)) == 0) of
+      true ->
+	SortedWeightedLines;
+      false ->
+	solve(lists:delete(element(1,hd(SortedWeightedLines)),Lines))
+    end.
+
+weight_all_lines([],_,Outs)->Outs;
+weight_all_lines(RemLines,Lines,Outs)->
+  InQ = hd(RemLines),
+  Weight = do_weight(InQ,Lines,0),
+  weight_all_lines(tl(RemLines),Lines,[{InQ,Weight}|Outs]).
+
+do_weight(_,[],Weight)->Weight;
+do_weight(InQ,Others,Weight)->
+  Other = hd(Others),
+  if
+    ( InQ#line.startIndex == Other#line.startIndex ) and
+    ( InQ#line.endIndex == Other#line.endIndex) ->
+      do_weight(InQ,tl(Others),Weight);
+    InQ#line.startIndex == Other#line.startIndex ->
+      do_weight(InQ,tl(Others),Weight+1);
+    InQ#line.endIndex == Other#line.endIndex ->
+      do_weight(InQ,tl(Others),Weight+1);
+    ( InQ#line.startIndex < Other#line.startIndex ) and
+    ( InQ#line.endIndex > Other#line.endIndex) ->
+      do_weight(InQ,tl(Others),Weight+1);
+    ( InQ#line.startIndex > Other#line.startIndex) and
+    ( InQ#line.endIndex < Other#line.endIndex) ->
+      do_weight(InQ,tl(Others),Weight+1);
+    true->
+      do_weight(InQ,tl(Others),Weight)
+  end.
+
+make_index_dict([],Outs,_)->Outs;
+make_index_dict(SeqB,Outs,Index)->
+  make_index_dict(tl(SeqB),dict:append_list(hd(SeqB),[Index],Outs),Index+1).
+
+build_all_lines([],_,_,Outs)->Outs;
+build_all_lines(SeqA,Index,SeqDict,Outs)->
+  NewLines = 
+    lists:foldl( fun(A,Acc)->
+	  [#line{startIndex=Index,endIndex=A}|Acc] end,[],dict:fetch(hd(SeqA),SeqDict)),
+  build_all_lines(tl(SeqA),Index+1,SeqDict,lists:merge(Outs,NewLines)).
 
 %===================== Internal ============================
 
