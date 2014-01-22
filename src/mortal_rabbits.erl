346413e81947ae02d1879e21c7d951808213cf21
sbirkett <supergrimey@gmail.com>
2014-01-22 13:13:04 -0600

    mortal rabbits started again

---
+++ src/mortal_rabbits.erl
@@ -0,0 +1,22 @@
+%% @author birketsc
+%% @doc @todo Add description to mortal_rabbits.
+
+-module(mortal_rabbits).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([solve/2]).
+
+solve(N,M)->
+  rec_iterate(N,M,0,[]).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+rec_iterate(Months,Litter,CurrMonth,Rabbits)->
+  1.
+
+iterate(Rabbits,Litter)->
+  1.
