2a69cc343915e0714ed8ff4e945da4407ac36ba7
scott <supergrimey@gmail.com>
2014-01-28 19:41:22 -0600

    more work on mortal rabbits

--- src/mortal_rabbits.erl
+++ src/mortal_rabbits.erl
@@ -8,15 +8,34 @@
 %% ====================================================================
 -export([solve/2]).
 
-solve(N,M)->
-  rec_iterate(N,M,0,[]).
+solve(N,K)->
+  rec_iterate(N-1,K,[K-1]).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-rec_iterate(Months,Litter,CurrMonth,Rabbits)->
-  1.
-
-iterate(Rabbits,Litter)->
-  1.
+rec_iterate(Months,TTL,Rabbits)->
+  io:format("rec_iterator\n",[]),
+  io:format("Months = ~w\n",[Months]),
+  io:format("TTL = ~w\n",[TTL]),
+  io:format("Rabbits = ~w\n",[Rabbits]),
+  % Age
+  AgedRabbits = [ A - 1 || A <- Rabbits ],
+  io:format("AgedRabbits = ~w\n",[AgedRabbits]),
+  % Filter and Breed
+  SurvivingRabbits =
+    lists:foldl( fun ( A, Acc) ->
+	  %case A of
+	  if
+	    A > TTL -> Acc;
+	    A == 0 -> Acc;
+	    true -> Acc ++ [A,TTL]
+	  end
+      end,[],AgedRabbits),
+  io:format("SR = ~w\n",[SurvivingRabbits]),
+  case Months of
+    1 -> length( AgedRabbits );
+    _ ->
+      rec_iterate(Months-1,TTL,SurvivingRabbits)
+  end.
