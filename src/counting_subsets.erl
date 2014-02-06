cdb442449792683d332bf2c024999ce8ab5ee4ce
scott <supergrimey@gmail.com>
2014-02-05 19:39:18 -0600

    added counting subsets

---
+++ src/counting_subsets.erl
@@ -0,0 +1,16 @@
+-module(counting_subsets).
+
+-export([solve/1]).
+
+solve(N)->
+  do_recurse(N,N,0).
+
+do_recurse(_,0,Out)->Out+1;
+do_recurse(K,N,Out)->
+  do_recurse(K,N-1,Out+binom(K,N)).
+
+binom(K,N)->
+  factorial(K) / ( factorial(N) * factorial(K-N)).
+  
+factorial(0)->1;
+factorial(N)->N * factorial(N-1).
