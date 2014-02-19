3f3576b9d7f6532b95aaffbb57a210f85e8cc806
sbirkett <supergrimey@gmail.com>
2014-02-19 16:45:16 -0600

    Stubs in for catalan

--- src/catalan_numbers.erl
+++ src/catalan_numbers.erl
@@ -10,3 +10,24 @@ solve(File)->
   compile:file("../fasta_reader.erl"),
   Fasta = hd(fasta_reader:read(File)),
   Seq = Fasta#fasta.sequence.
+
+
+% Make base start
+% All Graphs made from connecting edges that connect over an
+% outer span of 2*(k)
+make_base_set(Nodes)->
+  1.
+
+% Construct all the possible sub graphs
+make_valid_sub_graphs(Graph)->
+  1.
+
+% Check outside edges scenario
+% Two possible.
+outside_check(Nodes)->
+outside_check(Nodes)->
+  1.
+
+% Check if graph complete
+check_complete(Graph)->
+  1.
