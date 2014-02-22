ac19a12fd06331f352c1037b1f77fd91ff989eea
scott <supergrimey@gmail.com>
2014-02-22 09:53:29 -0600

    Working on catalan

--- src/catalan_numbers.erl
+++ src/catalan_numbers.erl
@@ -1,16 +1,28 @@
--module(catalan_numbers.erl).
+-module(catalan_numbers).
+
+-record(node,{value,ind,nxt}).
 
 -include_lib("../rosalind_records.hrl").
 
 -import(fasta_reader,[read/1]).
 
-export([solve/1]).
+-export([solve/1]).
 
 solve(File)->
   compile:file("../fasta_reader.erl"),
   Fasta = hd(fasta_reader:read(File)),
-  Seq = Fasta#fasta.sequence.
+  Seq = Fasta#fasta.sequence,
 
+  % Make nodes
+  Nodes = lists:foldl( fun (A,Acc) ->
+	Acc ++ [ 
+	  #node{
+	    value = A,
+	    ind = length(Acc) + 1,
+	    nxt = 0} ] end
+	,[],Seq),
+  outside_check(Nodes).
+  %check_complete(Nodes).
 
 % Make base start
 % All Graphs made from connecting edges that connect over an
@@ -19,15 +31,90 @@ make_base_set(Nodes)->
   1.
 
 % Construct all the possible sub graphs
-make_valid_sub_graphs(Graph)->
+make_valid_sub_graphs(Nodes)->
   1.
 
 % Check outside edges scenario
 % Two possible.
 outside_check(Nodes)->
-outside_check(Nodes)->
-  1.
+  NodeDict = dict:from_list([ { A#node.ind, A } || A <- Nodes ]),
+  EvenToOdd = dict:fold( fun(K,A,Acc) ->
+	case ( K rem 2 ) of
+	  1 -> 
+	    NextNode = get_next_elem(K,NodeDict),
+	    case valid_connection( A#node.value , NextNode#node.value ) of
+	      true -> lists:merge(Acc,assign_edge(A,NextNode));
+	      false -> lists:merge(Acc, [A,NextNode])
+	    end;
+	  _ ->
+	    Acc
+	end end,[],NodeDict),
+  OddToEven = dict:fold( fun(K,A,Acc) ->
+	case ( K rem 2 ) of
+	  0 ->
+	    NextNode=  get_next_elem(K,NodeDict),
+	    case valid_connection(A#node.value, NextNode#node.value) of 
+	      true -> lists:merge(Acc,assign_edge(A,NextNode));
+	      false -> lists:merge(Acc, [A,NextNode])
+	    end;
+	  _ ->
+	    Acc
+	end end,[],NodeDict),
+
+  case {check_complete(EvenToOdd),check_complete(OddToEven)} of
+    {true,true} -> 2;
+    {true,false} ->1;
+    {false,true} ->1;
+    {false,false} ->0
+  end.
+
 
 % Check if graph complete
-check_complete(Graph)->
-  1.
+check_complete(Nodes)->
+  % All Nodes have connections
+  EmptyNodes = lists:filter( fun (A) -> A#node.nxt == 0 end, Nodes),
+  case length(EmptyNodes) of 
+    0 ->
+      check_edges(Nodes,[]);
+    _ ->
+      false
+  end.
+
+% Intersecting Edges 
+check_edges([],_)->true;
+check_edges([Node|Nodes],[])-> check_edges(Nodes,[Node]);
+check_edges([Node|Nodes],CheckedNodes)->
+  Conflicts = lists:filter( fun(A) -> 
+	( (A#node.ind < Node#node.ind)
+	  and
+	  (A#node.nxt < Node#node.nxt)
+	)
+	or
+	( (A#node.ind > Node#node.ind)
+	  and
+	  (A#node.nxt > Node#node.ind)
+	) end, CheckedNodes),
+  case length(Conflicts) of
+    0 -> check_edges(Nodes,[Node|CheckedNodes]);
+    _ -> false
+  end.
+
+valid_connection(A,B)->
+  case {A,B} of 
+    {65,85} -> true;
+    {85,65} -> true;
+    {67,71} -> true;
+    {71,67} -> true;
+    {_,_} -> false
+  end.
+
+get_next_elem(CurrKey,Dict)->
+  case ( dict:size(Dict) == CurrKey ) of
+    true -> dict:fetch(1,Dict);
+    false -> dict:fetch(CurrKey+1,Dict)
+  end.
+
+assign_edge(A,B)->
+  AMod = #node { value = A#node.value, ind = A#node.ind, nxt = B#node.ind },
+  BMod = #node { value = B#node.value, ind = B#node.ind, nxt = A#node.ind },
+  [AMod,BMod].
